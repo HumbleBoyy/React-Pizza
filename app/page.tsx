c763760d32e9179c96338f322cffa67360d96af7
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 14:09:35 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Categories from "@/modules/Categories";
 import Header from "@/modules/Header";
 
 export default function Home() {
   return (
    <div className="max-w-[1340px] bg-white mt-10 rounded-[10px] block mx-auto p-10">
       <Header/>
+      <Categories/>
    </div>
   );
 }
--- modules/Categories.tsx
+++ modules/Categories.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import { getRequest } from '@/service/getRequest'
+import { CategoryType } from '@/types/CategoriesType'
+import ButtonUi from '@/ui/ButtonUi'
 import React from 'react'
 
 const Categories = () => {
+  const {data:categories, isLoading} = getRequest("/categories", "category")
+  console.log(categories)
   return (
-    <div>
-      Categories
+    <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-[10px]'>
+           {isLoading ? "Loading...." : categories.map((item:CategoryType)=> <ButtonUi key={item.id} extraClass={`rounded-[50px] hover:bg-transparent ${item.isActive ? "!bg-black" : "bg-[#F9F9F9] text-black"}`}>{item.name}</ButtonUi>)}
+        </div>
     </div>
   )
 }
