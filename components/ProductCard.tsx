8adce85fb705e0a007f46bacd583de7e3c709401
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 17:38:01 +0500

    New Commit

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,14 +4,27 @@ import { ProductType } from '@/types/ProductType'
 import ButtonUi from '@/ui/ButtonUi'
 import Image from 'next/image'
 import React, { FC } from 'react'
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const ProductCard:FC<{item:ProductType}> = ({item}) => {
   return (
     <div className='w-[280px] flex flex-col gap-2'>
        <Image className='w-[220px] h-[220px] mb-[11px] mx-auto' src={item.img} priority alt={item.title} width={220} height={220}/>
        <strong className=' font-bold text-[20px] text-center mx-auto block'>{item.title}</strong>
-       <div className='w-[280px] h-[85px] bg-[#F3F3F3] flex items-center justify-center'>
-           <strong className='text-[20px]'><span className='text-red-600'>!</span>Cooming Soon</strong>
+       <div className='w-[280px] py-2 bg-[#F3F3F3]  rounded-md'>
+       <Tabs defaultValue="тонкое" className="!w-[200px] !py-2">
+        <TabsList>
+          <TabsTrigger value="тонкое" className='!w-[130px]  text-[15px] font-bold cursor-pointer p-5'>тонкое</TabsTrigger>
+          <TabsTrigger value="традиционное" className='!w-[130px] text-[15px] font-bold cursor-pointer p-5'>традиционное</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <Tabs defaultValue="26 см." className="!w-[200px] !py-2">
+        <TabsList>
+          <TabsTrigger value="26 см." className='!w-[85px]  text-[15px] font-bold cursor-pointer p-5'>26 см.</TabsTrigger>
+          <TabsTrigger value="30 см." className='!w-[85px] text-[15px] font-bold cursor-pointer p-5'>30 см.</TabsTrigger>
+          <TabsTrigger value="40 см." className='!w-[85px] text-[15px] font-bold cursor-pointer p-5'>40 см.</TabsTrigger>
+        </TabsList>
+      </Tabs>
        </div>
        <div className='flex items-center justify-between'>
          <strong className='font-bold text-[22px]'>от {item.price} ₽</strong>
--- modules/Products.tsx
+++ modules/Products.tsx
@@ -9,7 +9,7 @@ const Products = () => {
   return (
     <div className='flex flex-col gap-10'>
       <h2 className='text-[32px] font-bold'>Все пиццы</h2>
-      <div className='flex flex-wrap justify-start items-center w-full gap-5 w-full'>{isLoading ? "Loading" : products.map((item:ProductType)=> <ProductCard key={item.id} item={item}/>)}</div>
+      <div className='flex flex-wrap justify-between  gap-5 '>{isLoading ? "Loading" : products.map((item:ProductType)=> <ProductCard key={item.id} item={item}/>)}</div>
     </div>
   )
 }
