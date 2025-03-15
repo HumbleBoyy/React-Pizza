bfb3ad63d2846e73ce5c760fddc7bad70c4be745
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 14:36:03 +0500

    New Commit

--- modules/Categories.tsx
+++ modules/Categories.tsx
@@ -3,15 +3,23 @@
 import { getRequest } from '@/service/getRequest'
 import { CategoryType } from '@/types/CategoriesType'
 import ButtonUi from '@/ui/ButtonUi'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Categories = () => {
-  const {data:categories, isLoading} = getRequest("/categories", "category")
- 
+  const {data, isLoading} = getRequest("/categories", "category")
+  const [categories, setCategories] = useState(data)
+
+  const handleCategories = (obj:CategoryType) => {
+     const request = {...obj, isActive:true}
+     setCategories(categories.map((item:CategoryType)=> item.id === obj.id ? request : {...item, isActive:false}))
+  }
+  useEffect(()=> {
+    setCategories(data)
+  }, [data])
   return (
     <div className='flex items-center justify-between'>
         <div className='flex items-center gap-[10px]'>
-           {isLoading ? "Loading...." : categories.map((item:CategoryType)=> <ButtonUi key={item.id} extraClass={`rounded-[50px] hover:bg-transparent ${item.isActive ? "!bg-black" : "bg-[#F9F9F9] text-black"}`}>{item.name}</ButtonUi>)}
+           {isLoading ? "Loading...." : categories.map((item:CategoryType)=> <ButtonUi key={item.id} onClick={()=> handleCategories(item)} extraClass={`rounded-[50px] hover:bg-transparent ${item.isActive ? "!bg-black" : "bg-[#F9F9F9] text-black"}`}>{item.name}</ButtonUi>)}
         </div>
     </div>
   )
