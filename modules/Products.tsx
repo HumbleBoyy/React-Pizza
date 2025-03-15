192abe03b4cbcd25abb52a2d8b0161fcf1c5f6c8
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 17:20:26 +0500

    New Commit

--- modules/Products.tsx
+++ modules/Products.tsx
@@ -6,11 +6,10 @@ import { ProductType } from '@/types/ProductType'
 
 const Products = () => {
     const {data:products, isLoading} = getRequest("/products", "products")
-    console.log(products)
   return (
     <div className='flex flex-col gap-10'>
       <h2 className='text-[32px] font-bold'>Все пиццы</h2>
-      <div className='flex flex-wrap gap-5 w-full'>{isLoading ? "Loading" : products.map((item:ProductType)=> <ProductCard key={item.id} item={item}/>)}</div>
+      <div className='flex flex-wrap justify-start items-center w-full gap-5 w-full'>{isLoading ? "Loading" : products.map((item:ProductType)=> <ProductCard key={item.id} item={item}/>)}</div>
     </div>
   )
 }
