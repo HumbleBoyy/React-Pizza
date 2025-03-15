3cb2866d934602398170dcdff95c70336051b639
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 18:55:48 +0500

    New Commi

---
+++ store/orderSlice.tsx
@@ -0,0 +1,26 @@
+import { ProductType } from '@/types/ProductType'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+
+interface OrderType {
+    orderList:ProductType[]
+}
+const initialState:OrderType = {
+    orderList: [],
+}
+
+export const orderSlice = createSlice({
+  name: 'order_list',
+  initialState,
+  reducers: {
+    saveOrderProduct: (state:OrderType, action:PayloadAction):OrderType | any => {
+       return {
+          orderList:[...state.orderList, action.payload]    
+       }
+    }
+  },
+})
+
+export const { saveOrderProduct } = orderSlice.actions
+
+export default orderSlice.reducer
