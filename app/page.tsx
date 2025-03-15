54b889cdddb04d1445beb2dec3ae2f73701e4eca
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 16:12:30 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Products from "@/modules/Products";
 
 export default function Home() {
   return (
-   <div className="max-w-[1340px] flex flex-col gap-3 bg-white mt-10 rounded-[10px] block mx-auto p-10">
+   <div className="max-w-[1340px] flex flex-col gap-3 bg-white mt-5 rounded-[10px] block mx-auto p-10">
       <Header/>
       <div  className="w-full h-[1px] bg-[#ededed]"/>
       <Categories/>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,13 +1,22 @@
 "use client"
+import { PlusIcon } from '@/assets'
 import { ProductType } from '@/types/ProductType'
+import ButtonUi from '@/ui/ButtonUi'
 import Image from 'next/image'
 import React, { FC } from 'react'
 
 const ProductCard:FC<{item:ProductType}> = ({item}) => {
   return (
-    <div className='w-[280px]'>
-       <Image className='w-[260px] h-[260px] mb-[11px]' src={item.img[0]} priority alt={item.title} width={260} height={260}/>
-       <strong className=' font-bold text-[20px] text-center'>{item.title}</strong>
+    <div className='w-[280px] flex flex-col gap-2'>
+       <Image className='w-[220px] h-[220px] mb-[11px] mx-auto' src={item.img} priority alt={item.title} width={220} height={220}/>
+       <strong className=' font-bold text-[20px] text-center mx-auto block'>{item.title}</strong>
+       <div className='w-[280px] h-[85px] bg-[#F3F3F3] flex items-center justify-center'>
+           <strong className='text-[20px]'><span className='text-red-600'>!</span>Cooming Soon</strong>
+       </div>
+       <div className='flex items-center justify-between'>
+         <strong className='font-bold text-[22px]'>от {item.price} ₽</strong>
+         <ButtonUi extraClass='!text-[16px] !rounded-[30px] !p-[10px] !bg-[#EB5A1E] border-2 border-[#EB5A1E]' bgColor={false} icon={<PlusIcon/>} iconPosition={"left"}>Добавить</ButtonUi>
+       </div>
     </div>
   )
 }
