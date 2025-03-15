7aed5dadc67c794f18d9908a172d96664b9ea5cf
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-15 14:55:04 +0500

    New Commit

--- modules/Categories.tsx
+++ modules/Categories.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ArrowIcon } from '@/assets'
 import { getRequest } from '@/service/getRequest'
 import { CategoryType } from '@/types/CategoriesType'
 import ButtonUi from '@/ui/ButtonUi'
@@ -9,7 +10,6 @@ const Categories = () => {
   const {data, isLoading} = getRequest("/categories", "category")
   const [categories, setCategories] = useState(data)
 
-  console.log(categories)
 
   const handleCategories = (obj:CategoryType) => {
      setCategories(categories?.map((item:CategoryType)=> item.id === obj.id ? {...obj, isActive:true} : {...item, isActive:false}))
@@ -21,6 +21,13 @@ const Categories = () => {
     <div className='flex items-center justify-between'>
         <div className='flex items-center gap-[10px]'>
            {isLoading ? "Loading...." : categories?.map((item:CategoryType)=> <ButtonUi key={item.id} onClick={()=> handleCategories(item)} extraClass={`rounded-[50px] hover:bg-transparent ${item.isActive ? "!bg-black" : "bg-[#F9F9F9] text-black"}`}>{item.name}</ButtonUi>)}
+        </div>
+        <div className='flex items-center gap-1'>
+            <div className='flex items-center gap-2'>
+              <ArrowIcon/>
+              <p className='font-bold text-[14px]'>Сортировка по:</p>
+            </div>
+            <strong className='font-semibold text-[14px] text-[#FE5F1E]'>популярности</strong>
         </div>
     </div>
   )
