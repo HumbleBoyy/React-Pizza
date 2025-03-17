e4ab320083f60f51a1c96ff603dd6b4bd6cfa0bf
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-17 10:03:18 +0500

    New Commit

--- modules/Header.tsx
+++ modules/Header.tsx
@@ -7,14 +7,14 @@ import { useSelector } from 'react-redux'
 import { ProductType } from '@/types/ProductType'
 
 const Header = () => {
-  const orderCount = useSelector(state => state)
+  const orderCount = useSelector((state:{orderList:ProductType[]}) => state.orderList)
   return (
     <div className='w-full flex justify-between items-center py-5'>
       <Image src={MainLogo} alt='ReactPzza'/>
       <ButtonUi extraClass='bg-[#FE5F1E] rounded-[30px] w-[150px] h-[50px] text-white hover:bg-[#FE5F1E]'>
           <strong className='text-[16px] font-bold flex items-center gap-1'>0 ₽</strong>
           <div className='h-[25px] w-[1px] bg-[#FFFFFF40]'/>
-          <strong className='text-[16px] font-bold flex items-center gap-1'><Cart/>{0}</strong>
+          <strong className='text-[16px] font-bold flex items-center gap-1'><Cart/>{orderCount.length}</strong>
       </ButtonUi>
     </div>
   )
