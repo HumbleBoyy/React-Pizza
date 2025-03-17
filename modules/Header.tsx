b79b3552213dc0adc25229af175f34a535a235aa
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-17 10:02:01 +0500

    New Commit

--- modules/Header.tsx
+++ modules/Header.tsx
@@ -4,6 +4,7 @@ import MainLogo from "../public/mainLogo.svg"
 import ButtonUi from '@/ui/ButtonUi'
 import { Cart } from '@/assets'
 import { useSelector } from 'react-redux'
+import { ProductType } from '@/types/ProductType'
 
 const Header = () => {
   const orderCount = useSelector(state => state)
@@ -13,7 +14,7 @@ const Header = () => {
       <ButtonUi extraClass='bg-[#FE5F1E] rounded-[30px] w-[150px] h-[50px] text-white hover:bg-[#FE5F1E]'>
           <strong className='text-[16px] font-bold flex items-center gap-1'>0 ₽</strong>
           <div className='h-[25px] w-[1px] bg-[#FFFFFF40]'/>
-          <strong className='text-[16px] font-bold flex items-center gap-1'><Cart/>0</strong>
+          <strong className='text-[16px] font-bold flex items-center gap-1'><Cart/>{0}</strong>
       </ButtonUi>
     </div>
   )
